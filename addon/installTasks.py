efe3452f312b287aa5062a053a8f207f1d479f01
Łukasz Golonka <lukasz.golonka@mailbox.org>
2020-08-08 22:16:36 +0200

    installTasks: Copyright and Lint updates

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -1,23 +1,25 @@
 # -*- coding: UTF-8 -*-
 
 # Install tasks for Percentage checker add-on
-#Copyright (C) 2019 Łukasz Golonka <lukasz.golonka@mailbox.org>
+# Copyright (C) 2019-2020 Łukasz Golonka <lukasz.golonka@mailbox.org>
 # Released under GPL 2
 
 
 import addonHandler
-import gui
-import wx
-
-
 addonHandler.initTranslation()
+
 
 def onInstall():
 	for addon in addonHandler.getAvailableAddons():
 		if addon.name == "jump to line":
+			import gui
+			import wx
 			gui.messageBox(
-				# Translators: Content of the dialog informing about presence of old no longer needed add-on.
-				_("Functionality of The jump to line add-on that you have installed is now included in the percentage checker. After pressing OK it would be removed."),
+				_(
+					# Translators: Content of the dialog informing about presence of old no longer needed add-on.
+					"Functionality of The jump to line add-on that you have installed "
+					"is now included in the percentage checker. After pressing OK it would be removed."
+				),
 				# Translators: Title of the dialog
 				_("Information"),
 				wx.OK
